f1b27bc871a310de97d7470cfc20b252313859b9
jmanock <jonmanock@gmail.com>
2015-10-27 18:55:20 -0400

    testing out older versions

--- js/background.js
+++ js/background.js
@@ -1,62 +1,106 @@
-(function(){
-  var FeedReader = function(){
-    var feedUrl = 'https://www.sports.yahoo.com/nhl/scorboard';
-    var newsHolder = $('.news-feed');
-    var beforeFeed = '<ul>';
-    var afterFeed = '</ul>';
-    var beforeFeedItem = '<li>';
-    var afterFeedItem = '</li>';
-
-    function performAjaxSetup(){
-      $(document).ajaxStart(function(){
-        $('#reload').html('Loading.. Please Wait');
-        $('#reload').off();
-      });
-
-      $(document).ajaxComplete(function(event, xhr, settings){
-        $('#reload').html('Reload');
-        feed.bindUI();
-      });
-    }
-
-    var feed ={
-      xml:'',
-      init:function(){
-        this.bindUI();
-        performAjaxSetup();
-        this.loadFeed();
-      },
-      bindUI:function(){
-        $('#reload').on('click', function(){
-          feed.loadFeed();
-        });
-      },
-      loadFeed:function(){
-        this.fetchFeed();
-      },
-      fetchFeed:function(){
-        newsHolder.empty();
-        $.ajax({
-          url:feedUrl,
-          dataType:'html',
-          method:'post',
-          beforeSend:function(){},
-          success:function(xml){
-
-            feed.populateExt(xml);
-          },
-          complete:function(){},
-        });
-      },
-      populateExt:function(xml){
-        var anchors = beforeFeed;
-        
-      }
-    };
-    return feed;
-  };
-  $(function(){
-    var Feed = new FeedReader();
-    Feed.init();
+$(document).ready(function(){
+  $.ajax({
+    url:'http://www.thescore.com/nhl/events/day/'
+  }).done(function(html){
+    console.log(html);
+    globalHtml = html;
+    var date = getDateString();
+    var find = '#'+date+'.games';
+    offset =0;
+    showScores(html,date);
   });
-})();
+  function showScores(html, date){
+    $('#container').css('width', 'auto');
+    $('#games').empty();
+    var find = '#'+date+'.games';
+    $('p.date').empty();
+    $('p.date').append(fanciy(date));
+    var f = $(html).find(find);
+    var str = $(html).find(find).each(function(i,el){
+      $(this).find('.gamelist').each(function(i1,el1){
+        var src = '/pp.png';
+        $(el1).find('.go').remove();
+        $('img[alt="Nhl_icon_powerplay"]').attr('src',src);
+        $('#games').append(el1);
+      });
+    });
+    if(!f.length){
+      $('#games').append('No Games to display');
+    }
+  }
+  function getDateString(offset){
+    offset = offset || 0;
+    var d = getOffsetDayNum(offset);
+    var m = getMonthString(d.getMonth());
+    var day = d.getDate();
+    if(day < 10){
+      day = '0'+day;
+    }
+    var date = m +'_'+day;
+    return date;
+  }
+  function fancify(date){
+    var m_names = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    var d = getOffsetDayNum(offset);
+    var m = d.getMonth();
+    var month = m_names[m];
+    var day = date.substr(date.length -2);
+    return month + ' '+day;
+  }
+  function getOffsetDayNum(offset){
+    var d = new Date();
+    d.setDate((d.getDate())+offset);
+    return d;
+  }
+  function getMonthDtring(monthNum){
+    var n = monthNum + 1;
+    var month = '';
+    switch(n){
+      case 1:
+      month = 'jan';
+      break;
+      case 2:
+      month = 'feb';
+      break;
+      case 3:
+      month = 'mar';
+      break;
+      case 4:
+      month = 'apr';
+      break;
+      case 5:
+      month = 'may';
+      break;
+      case 6:
+      month = 'jun';
+      break;
+      case 7:
+      month = 'jul';
+      break;
+      case 8:
+      month = 'aug';
+      break;
+      case 9:
+      month = 'sep';
+      break;
+      case 10:
+      month = 'oct';
+      break;
+      case 11:
+      month = 'nov';
+      break;
+      case 12:
+      month = 'dec';
+      break;
+    }
+    return month;
+  }
+  $('a.navNext').click(function(){
+    offset = offset +1;
+    showScores(globalHtml, getDateString(offset));
+  });
+  $('a.navPrev').click(function(){
+    offset = offset -1;
+    showScores(globalHtml, getDateString(offset));
+  });
+});
