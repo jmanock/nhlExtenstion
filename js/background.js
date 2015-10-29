101117e58aaebf5ec0949fdcb09f912ac2451155
jmanock <jonmanock@gmail.com>
2015-10-29 05:43:15 -0400

    cleaned up the code

--- js/background.js
+++ js/background.js
@@ -3,12 +3,10 @@
     url:'http://sports.yahoo.com/nhl/scoreboard/'
   }).done(function(html){
     $(html).find('.game').each(function(i,e){
-        $(e).find('.away, .home').each(function(ind,elem){
-          $(elem).find('em').each(function(index,element){
-            var something = $(element).text();
-            console.log(something);
-          });
-        });
+      console.log(e);
+      var awayTeam = $(e).find('.away em').text();
+      var homeTeam = $(e).find('.home em').text();
+      console.log(awayTeam + ' at ' + homeTeam);
     });
 
   });
