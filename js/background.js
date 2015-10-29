822c7edd59d6f7cc6e118b9416f09a57eb85b136
jmanock <jonmanock@gmail.com>
2015-10-29 18:21:10 -0400

    got the games to show up in order

--- js/background.js
+++ js/background.js
@@ -2,12 +2,16 @@
   $.ajax({
     url:'http://sports.yahoo.com/nhl/scoreboard/'
   }).done(function(html){
+    var open = '<ul>';
+    var close = '</ul>';
     $(html).find('.game').each(function(i,e){
       var time = $(e).find('.time').text();
       var awayTeam = $(e).find('.away em').text();
       var homeTeam = $(e).find('.home em').text();
-      $('#games').append(time +' '+awayTeam+' at '+homeTeam);
+      var something = '<li>'+time+' '+awayTeam+' at '+homeTeam+'</li>';
+      open +=something;
     });
-
+    open+=close;
+    $('#games').append(open);
   });
 })();
