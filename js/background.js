fe4f8740492878b20da1c935d6bcc616b4b23c1c
jmanock <jonmanock@gmail.com>
2015-10-29 18:57:22 -0400

    everything looks right, need to work on changing the date

--- js/background.js
+++ js/background.js
@@ -8,7 +8,9 @@
       var time = $(e).find('.time').text();
       var awayTeam = $(e).find('.away em').text();
       var homeTeam = $(e).find('.home em').text();
-      var something = '<li>'+time+' '+awayTeam+' at '+homeTeam+'</li>';
+      var score = $(e).find('.score a').text();
+
+      var something = '<li>'+time+' '+awayTeam+' '+score+' '+homeTeam+'</li>';
       open +=something;
     });
     open+=close;
