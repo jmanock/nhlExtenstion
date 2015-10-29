68e7185e18846e78dc5d581668e72e68b5db9ac9
jmanock <jonmanock@gmail.com>
2015-10-29 05:46:23 -0400

    time team vs team

--- js/background.js
+++ js/background.js
@@ -3,10 +3,10 @@
     url:'http://sports.yahoo.com/nhl/scoreboard/'
   }).done(function(html){
     $(html).find('.game').each(function(i,e){
-      console.log(e);
+      var time = $(e).find('.time').text();
       var awayTeam = $(e).find('.away em').text();
       var homeTeam = $(e).find('.home em').text();
-      console.log(awayTeam + ' at ' + homeTeam);
+      console.log(time + ' '+awayTeam + ' at ' + homeTeam);
     });
 
   });
