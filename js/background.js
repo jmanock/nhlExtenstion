c8d5bda3c6328875860c442af3edd14e724394a9
jmanock <jonmanock@gmail.com>
2015-10-29 05:38:40 -0400

    got text out

--- js/background.js
+++ js/background.js
@@ -2,8 +2,13 @@
   $.ajax({
     url:'http://sports.yahoo.com/nhl/scoreboard/'
   }).done(function(html){
-    var something =$(html).find('.game').each(function(i,e){
-        console.log(i,e);
+    $(html).find('.game').each(function(i,e){
+        $(e).find('.away, .home').each(function(ind,elem){
+          $(elem).find('em').each(function(index,element){
+            var something = $(element).text();
+            console.log(something);
+          });
+        });
     });
 
   });
