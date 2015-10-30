af537c7c3a1675887b5c0206d8bc0befecf76613
jmanock <jonmanock@gmail.com>
2015-10-30 10:45:10 -0400

    working on the nav links to change the date

--- js/background.js
+++ js/background.js
@@ -13,6 +13,28 @@
       var something = '<li>'+time+' '+awayTeam+' '+score+' '+homeTeam+'</li>';
       open +=something;
     });
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+
+    if(dd < 10){
+      dd = '0'+dd;
+    }
+    if(mm<10){
+      mm='0'+mm;
+    }
+    today = yyyy+'-'+mm+'-'+dd;
+    console.log(today);
+    $('#next').on('click', function(){
+      dd = dd +1;
+      console.log(today,dd);
+    });
+
+    $('#prev').on('click', function(){
+
+    });
     open+=close;
     $('#games').append(open);
   });
