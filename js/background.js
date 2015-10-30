b862d88d1434967a1fccd5d01a9acb3ae2bc8293
jmanock <jonmanock@gmail.com>
2015-10-30 13:48:29 -0400

    working on a new way to get the date on button clicks

--- js/background.js
+++ js/background.js
@@ -1,53 +1,15 @@
 (function(){
-  var urlPage = 'http://sports.yahoo.com/nhl/scoreboard/';
-  $.ajax({
-    url:urlPage
-  }).done(function(html){
-    var open = '<ul>';
-    var close = '</ul>';
-    $(html).find('.game').each(function(i,e){
-      var time = $(e).find('.time').text();
-      var awayTeam = $(e).find('.away em').text();
-      var homeTeam = $(e).find('.home em').text();
-      var score = $(e).find('.score a').text();
-
-      var something = '<li>'+time+' '+awayTeam+' '+score+' '+homeTeam+'</li>';
-      open +=something;
-    });
-
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;
-    var yyyy = today.getFullYear();
-
-    if(dd < 10){
-      dd = '0'+dd;
-    }
-    if(mm<10){
-      mm='0'+mm;
-    }
-    today = yyyy+'-'+mm+'-'+dd;
-    console.log(today);
-    $('#next').on('click', function(){
-      dd = dd +1;
-      if(dd ===32){
-        dd = 1;
-        mm = mm+1;
-      }
-      if(dd < 10){
-        dd = '0'+dd;
-      }
-      if(mm < 10){
-        mm = '0'+mm;
-      }
-      today = yyyy+'-'+mm+'-'+dd;
-      console.log(urlPage+'?date='+today);
-    });
-
-    $('#prev').on('click', function(){
-
-    });
-    open+=close;
-    $('#games').append(open);
-  });
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
+  if(dd < 10){
+    dd = '0'+dd;
+  }
+  if(mm < 10){
+    mm = '0'+mm;
+  }
+  today = yyyy+ '-'+mm+'-'+dd;
+  var Url = 'http://sports.yahoo.com/nhl/scoreboard/?date='+today;
+  console.log(Url);
 })();
